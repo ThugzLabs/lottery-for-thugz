a53557f766b0778fb3a1c6d0ddf60981111ae885
Thugz Life <thugz.nft@gmail.com>
2022-04-04 02:32:49 +0200

    Adding letter animation result reveal

--- src/App.js
+++ src/App.js
@@ -27,11 +27,31 @@ const listing = [];
   <div className="lottery">
     <div class="logo"><img src="https://ranking.thugz.life/static/media/ThugzNFT-Logo.c5feeed78011eb150d8a.png" alt="Thugz Life NFT Logo"></img></div>
     <h1 class="title gradient-text">Thugz Lottery</h1>
-    <div class="result">{winner !== null ?  winner : null}</div>
 
+    <div class="result">
+
+      <div class="letter-image">
+        <div class="animated-mail">
+          <div class="back-fold"></div>
+          <div class="letter">
+            <div class="letter-border"></div>
+            <div class="letter-title">Wallet</div>
+            <div class="letter-context">{winner !== null ?  winner : null}</div>
+            <div class="letter-stamp">
+              <div class="letter-stamp-inner"></div>
+            </div>
+          </div>
+          <div class="top-fold"></div>
+          <div class="body"></div>
+          <div class="left-fold"></div>
+        </div>
+        <div class="shadow"></div>
+      </div>
+      
+    </div>
     <input onClick={lance} type="checkbox" name="button" id="button" />
     <label class="bevel" for="button">We have our Winner !</label>
-    <span>DRAW a WINNER</span>
+    <span>Draw a Winner</span>
   </div>
   )
 }
